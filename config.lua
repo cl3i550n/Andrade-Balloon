84eef111940d067be9b30f3eee65313959b3f135
clandrade <cleisson@outlook.com.br>
2024-02-08 14:49:56 -0300

    updates

--- config.lua
+++ config.lua
@@ -1,16 +1,20 @@
 Config = {}
 
+-- Language settings 🌍
 Config.Lang = 'English'
 
-Config.KeyToBuyBalloon = 0xD9D0E1C0 -- [ SPACE ]
+-- Interaction key settings 🗝️
+Config.KeyToBuyBalloon = 0xD9D0E1C0 -- [ SPACE ] Key to initiate balloon purchase
 
-Config.EnableTax = true
-Config.BallonPrice = 10.00 -- Needs [ EnableTax = true ]
+-- Pricing and tax settings 💰
+Config.EnableTax = true  -- Toggle to enable/disable tax on purchases
+Config.BallonPrice = 10.00 -- Base price for balloons (tax applied if EnableTax is true)
 
+-- Balloon purchase locations 🎈
 Config.BalloonLocations = {
   {
-      coords = vector3(-396.19, 716.79, 115.88),
-      name = "Balloon Valentine"
+      coords = vector3(-396.19, 716.79, 115.88), -- Coordinates for the balloon vendor
+      name = "Balloon Valentine" -- Name or description of the balloon vendor location
   },
-  -- Add more here
+  -- Add more locations here using the same format
 }
